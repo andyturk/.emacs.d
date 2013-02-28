037ae5514ae3d581d9b5dd40c1a9582904dca358
Andy Turk <andyturk@yahoo.com>
2013-02-28 08:24:18 -0800

    include solarized theme

--- emacs-startup.el
+++ emacs-startup.el
@@ -31,6 +31,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
  '(exec-path (quote ("/usr/bin" "/bin" "/usr/sbin" "/sbin" "/Applications/Emacs.app/Contents/MacOS/bin" "/Users/andy/yagarto-4.7.1/bin" "/usr/local/bin")))
  '(gud-gdb-command-name "arm-none-eabi-gdb -i=mi")
  '(inhibit-startup-screen t)
@@ -46,9 +47,8 @@
 (prefer-coding-system 'utf-8)
 
 ;; By an unknown contributor
-
 (require 'whitespace)
-(setq whitespace-style '(face empty tabs lines-tail trailing))
+(setq whitespace-style '(face empty lines-tail trailing))
 
 (require 'fill-column-indicator)
 (defvar auto-minor-mode-alist
